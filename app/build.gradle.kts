7be2b88f730a2496247136dc948a657e1cdbc7b7
SirYadav1 <sundramy807@gmail.com>
2026-08-07 21:55:52 +0530

    chore: bump version to 1.5.0 (code 9) for pastel theme release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.quizforge.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.4.0"
+        versionCode = 9
+        versionName = "1.5.0"
     }
 
     buildTypes {
